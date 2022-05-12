aced744ee5fe8db55ae3f235c356e3413935f643
Muhammad Rehan Azaz <89381601+rehanazaz@users.noreply.github.com>
2022-05-12 16:53:12 +0500

    popup dynamic location

--- js-script.js
+++ js-script.js
@@ -578,6 +578,7 @@
 
                     if(fbMessengerPosition == 'left')
                     {
+                        ccFbMRJquery('head').append('<script type="text/css">.fb_iframe_widget iframe{left: 35px !important;}</script>')
                         ccFbMRJquery('head').append('<style type="text/css">.fb-customerchat.fb_invisible_flow.fb_iframe_widget iframe {left: 30px !important;}.fb_dialog {position: -webkit-sticky !important;position: fixed !important;left: 30px !important;} .fb_dialog_content iframe {left: 45px !important;}</style>');
                         console.log("New styling loaded ... Messenger on left side of screen");
                     }
